640512606df53b9fe1ae91fdfe203c9238579175
Andrew Lampert <andrew@lampert.ca>
2015-11-29 00:09:17 -0500

    making sure BT is enabled

--- app/src/main/java/solutions/lampert/strangerdanger/backend/Connection.java
+++ app/src/main/java/solutions/lampert/strangerdanger/backend/Connection.java
@@ -1,6 +1,6 @@
 package solutions.lampert.strangerdanger.backend;
 
-import android.bluetooth.BluetoothDevice;
+import android.bluetooth.*;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,17 +8,36 @@ import android.content.IntentFilter;
 
 import java.util.ArrayList;
 
+import static android.support.v4.app.ActivityCompat.startActivityForResult;
+
 /**
  * Created by andrew on 11/28/15.
  */
 public class Connection {
-
     private ArrayList<String> m_devices;
-    private String m_deviceText;
 
     void updateDeviceList(){
         // Send query to server to get a list of devices
     }
 
+    String getDeviceList(){
+        String deviceText = "";
 
+        // Getting bluetooth adapter
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        }
+
+        // Making sure BT is enabled
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+
+
+        return deviceText;
+    }
 }
