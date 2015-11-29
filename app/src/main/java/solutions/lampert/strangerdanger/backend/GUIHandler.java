b626d0089540438778caf27c6db4e19df0b04348
Andrew Lampert <andrew@lampert.ca>
2015-11-29 02:30:25 -0500

    trying to get GUIHandler to work, but its not. Not using it anymore

--- app/src/main/java/solutions/lampert/strangerdanger/backend/GUIHandler.java
+++ app/src/main/java/solutions/lampert/strangerdanger/backend/GUIHandler.java
@@ -1,11 +1,29 @@
 package solutions.lampert.strangerdanger.backend;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import solutions.lampert.strangerdanger.OpponentsView;
+
 /**
  * Created by andrew on 11/29/15.
  */
 public class GUIHandler {
 
-    public void changeViewHandler(){
+    public View.OnClickListener changeViewHandler(final Button btn, final AppCompatActivity act1, final AppCompatActivity act2){
+        View.OnClickListener handler = new View.OnClickListener(){
+            public void onClick(View v){
+                if (v == btn){
+//                    Intent intentMain = new Intent(MainActivity.this, OpponentsView.class);
+//                    MainActivity.this.startActivity(intentMain);
+//                    Log.i("Content ", " Main layout ");
+                }
+            }
+        };
 
+        return handler;
     }
 }
