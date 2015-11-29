d006686612f7941aef1032208b72a66fade677a3
Andrew Lampert <andrew@lampert.ca>
2015-11-28 23:40:43 -0500

    adding basic Connection class

---
+++ app/src/main/java/solutions/lampert/strangerdanger/backend/Connection.java
@@ -0,0 +1,15 @@
+package solutions.lampert.strangerdanger.backend;
+
+import java.util.ArrayList;
+
+/**
+ * Created by andrew on 11/28/15.
+ */
+public class Connection {
+
+    private ArrayList<String> m_devices;
+
+    void updateDeviceList(){
+        // Send query to server to get a list of devices
+    }
+}
