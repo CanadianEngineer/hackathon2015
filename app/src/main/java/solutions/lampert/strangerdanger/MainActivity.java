8878b0af98e6fdfe4aefc9ef9c8173f9dd46ddb2
Andrew Lampert <andrew@lampert.ca>
2015-11-29 02:18:19 -0500

    button to switch views not totally working

--- app/src/main/java/solutions/lampert/strangerdanger/MainActivity.java
+++ app/src/main/java/solutions/lampert/strangerdanger/MainActivity.java
@@ -1,7 +1,11 @@
 package solutions.lampert.strangerdanger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import solutions.lampert.strangerdanger.backend.Connection;
@@ -22,5 +26,21 @@ public class MainActivity extends AppCompatActivity {
         TextView deviceList = (TextView)findViewById(R.id.testText);
 //        deviceList.setText("Text to set");
         c.getDeviceList(deviceList);
+
+        // Setting button action listeners
+
+        final Button btn_toOpponentsView = (Button) findViewById(R.id.btn_viewOpponents);
+
+
+        // Listeners
+        View.OnClickListener handler_toOpponentsView = new View.OnClickListener(){
+            public void onClick(View v){
+                if (v == btn_toOpponentsView){
+                    Intent intentMain = new Intent(MainActivity.this, OpponentsView.class);
+                    MainActivity.this.startActivity(intentMain);
+                    Log.i("Content ", " Main layout ");
+                }
+            }
+        };
     }
 }
