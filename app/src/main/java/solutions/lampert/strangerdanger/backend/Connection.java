505980ec1cb028bf102ff63910d08c12ca6f6eee
Andrew Lampert <andrew@lampert.ca>
2015-11-28 23:45:30 -0500

    copy pasta searching devices

--- app/src/main/java/solutions/lampert/strangerdanger/backend/Connection.java
+++ app/src/main/java/solutions/lampert/strangerdanger/backend/Connection.java
@@ -1,5 +1,11 @@
 package solutions.lampert.strangerdanger.backend;
 
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
 import java.util.ArrayList;
 
 /**
@@ -12,4 +18,21 @@ public class Connection {
     void updateDeviceList(){
         // Send query to server to get a list of devices
     }
+
+    // Create a BroadcastReceiver for ACTION_FOUND
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    };
+    // Register the BroadcastReceiver
+    IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+    registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
 }
