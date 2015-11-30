b1df6bada55e88a3ea2a1e11eb99b4c6b5d1dae7
Andrew Lampert <andrew@lampert.ca>
2015-11-30 01:59:52 -0500

    adding basic game - press yes to win

---
+++ app/src/main/java/solutions/lampert/strangerdanger/game_press_yes.java
@@ -0,0 +1,13 @@
+package solutions.lampert.strangerdanger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class game_press_yes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_press_yes);
+    }
+}
